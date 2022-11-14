8bfa64cfdfd53884111b60bbbf7201480417462f
Zoclhas <70476453+Zoclhas@users.noreply.github.com>
2022-11-14 21:05:02 +0400

    Init all required packages

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { ThemeSwitch } from "../components/ThemeSwitch";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <ThemeSwitch />
+        </div>
+    );
+}
