da67ed17670360441fe82febf50377ac868a7b7e
Zoclhas <70476453+Zoclhas@users.noreply.github.com>
2022-11-15 19:31:27 +0400

    Getting started on BE views and urls

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = ["/gallery"]
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getImages(request):
+    images = ["hi", "hi 2", "hi 3"]
+    return Response(images)
